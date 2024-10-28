0f442a3188086e8bf5ebcdab08250a9f7aa3efaf
Abhinav <abhi9780244826@gmail.com>
2024-10-28 13:35:37 +0530

    async function practice

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,21 @@
 */
 
 function wait(n) {
+    return new Promise(resolve => {
+        setTimeout(resolve,n*1000);
+    });
+
+    function xoxox(duration){
+        const p = new Promise(function(resolve){
+            setTimeout(resolve,duration);
+        });
+        return p;
+    }
+
+    xoxox(1000).then(function(){
+        console.log(doen);
+    })
 }
+
 
 module.exports = wait;
